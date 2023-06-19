05f61534b258f3205c97f9651428875a223f4225
AbigailXu <qbxzz233@gmail.com>
2023-06-19 11:08:20 -0400

    create basic route

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mysql = require('mysql');
 
-// create connection
+// routers //
+const musicRouter = require('./routes/musicRoute');
+
+// db connection //
 const db = mysql.createConnection({
     host: 'us-cdbr-east-06.cleardb.net',
     user: 'b821f9605c65b7',
-    password: '7dc64a05',
-    // database: 'cleardb-concave-15040'
+    password: '7dc64a05'
 });
 
 db.connect((err) => {
@@ -16,7 +18,16 @@ db.connect((err) => {
     console.log('MySql Connected ...');
 })
 
+// app //
+
 const app = express();
+
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+
+
+app.use("/", musicRouter);
+
 
 app.listen('3000', () => {
     console.log("Server started on port 3000")
---
+++ controllers/musicCtrl.js
@@ -0,0 +1,13 @@
+
+
+
+// @desc        Get Top 10 songs by Rank
+// @route       GET /getTop10
+// @access      Public
+exports.getTop10 = async (req, res) => {
+    
+    res.status(200).json({
+        "Data:": "placeholder"
+    });
+};
+
