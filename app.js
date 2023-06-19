e5b706b0bbcc0d3e15ff7cf6317862214ee4587c
AbigailXu <qbxzz233@gmail.com>
2023-06-19 17:07:31 -0400

    env port

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { dbInit } = require('./database/db_init');
 const mysql = require('mysql');
 
+const port = process.env.PORT || 3000;
+
 // routers //
 const musicRouter = require('./routes/musicRoute');
 
@@ -18,6 +20,6 @@ app.use(express.urlencoded()); //Parse URL-encoded bodies
 
 app.use("/", musicRouter);
 
-app.listen('3000', () => {
-    console.log("Server started on port 3000")
-})
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+  });
