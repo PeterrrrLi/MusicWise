ec3c052364957c12c17685d1157d5190639a56d7
AbigailXu <qbxzz233@gmail.com>
2023-07-08 14:18:59 -0400

    add endpoints

--- src/routes/musicRoute.js
+++ src/routes/musicRoute.js
@@ -5,6 +5,9 @@ const MusicCtrl = require('../controllers/musicCtrl')
 const router = express.Router()
 
 router.get('/getTop10', MusicCtrl.getTop10)
+router.get('/getTop10Artists', MusicCtrl.getTop10Artists)
+router.get('/getAllSongs', MusicCtrl.getAllSongs)
+
 router.get('/hello', MusicCtrl.hello)
 
 module.exports = router;
