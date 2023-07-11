692371ade6a9263714cbc2643e06c2f1135ccf9b
Yibo-Ma <mybstudy@gmail.com>
2023-07-11 22:56:19 +0800

    Add: Add backend for Search

--- src/routes/musicRoute.js
+++ src/routes/musicRoute.js
@@ -6,10 +6,10 @@ const router = express.Router()
 
 router.get('/getTop10FanRank', MusicCtrl.getTop10FanRank)
 router.get('/getTop10SpotifyRank', MusicCtrl.getTop10SpotifyRank)
-router.get('/getTop10FanArtists', MusicCtrl.getTop10FanArtists)
-router.get('/getTop10SpotifyArtists', MusicCtrl.getTop10SpotifyArtists)
+router.get('/getTop10Artists', MusicCtrl.getTop10Artists)
 router.get('/getAllSongs', MusicCtrl.getAllSongs)
 router.post('/insertUserRanking', MusicCtrl.insertUserRanking)
+router.get('/search', MusicCtrl.search); 
 router.get('/hello', MusicCtrl.hello)
 
 module.exports = router;
